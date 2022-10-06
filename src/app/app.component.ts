8b43ce6a4ea9fa823d747618d2226f32f74ffbdb
Anna Boval <anna.bo1106@gmail.com>
2022-10-06 16:26:14 +0200

    added new class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {POKEMONS} from './mock-pokemon-list'
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   // vues définies dans le template, syst encapsylé 
   template: `
       <h1>
-        Welcome to {{title}}!
+        Liste de Pokemons
       </h1>
-      <p> {{pokemonList}} </p>`
+      <!-- <ul>
+        <li>{{pokemonList}}</li>
+      </ul> -->     `
 })
 
 export class AppComponent implements OnInit {
   // propriétés, méthodes, logique décrite dans la classe
-  title = 'appli de Pokemons';
-  pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+  // on bloque l'objet pokemonList 
+  pokemonList: Pokemon[] = POKEMONS;
 
   constructor() {
    // interdit de déclarer ici: this.pokemonList=[];
-
   }
 
   ngOnInit() {
     // par défaut void : méthode vide 
  console.table(this.pokemonList); 
- 
-  };
-}
+ this.selectPokemon(this.pokemonList[2]);
+  }
+  // méthode 
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`vous avez cliqué sur le pokémon ${pokemon.name}`);  
+  }
+  }
